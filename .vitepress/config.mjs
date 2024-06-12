fd02c81c4df8d5ea2aab7230ed8df537b43a8746
wzf <1902107289@qq.com>
2024-06-12 11:03:02 +0800

    first-commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from './utils/auto_sidebar.js'
+export default defineConfig({
+  title: "关于收集插件的用法",
+  description: "A VitePress Site",
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  themeConfig: {
+    logo: '/logo.svg',
+    outlineTitle: '目录',
+    outline: [1, 6],
+    nav: [
+      {
+        text: '首页', items: [
+          { text: '尚医通', link: 'http://8.134.184.87' }
+        ]
+      },
+      {
+        text: '示例', items: [
+          { text: 'pdfjs', link: '/toolsfunction/pdfjs/' }
+        ]
+      }
+    ],
+
+    sidebar: {
+      "/toolsfunction/pdfjs": set_sidebar("/toolsfunction/pdfjs")
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      copyright: 'Copyright @2024 Author WEI'
+    },
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "关于收集插件的用法"
+  text: "灵感和想法"
+  tagline: 持续学习，持续进步
+  image:
+    src: /bg.png
+    alt: 背景图片
+  actions:
+    - theme: brand
+      text: pdfjs
+      link: /markdown-examples
+    - theme: alt
+      text: threejs
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
